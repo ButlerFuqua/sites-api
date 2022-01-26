dce6916d384ebadd219d68f0e75a5af87bbfc600
ButlerFuqua <butlerfuqua@gmail.com>
2022-01-26 10:23:02 -1000

    Add stripe checkout subscription boiler

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -14,5 +14,7 @@ router.use(`/comments`, require('./commentController'))
 router.use(`/subscriber`, require('./subscriberController'))
 // Newsletter
 router.use(`/newsletter`, require('./newsletterController'))
+// Payments
+router.use(`/payment`, require('./paymentController'))
 
 module.exports = router
