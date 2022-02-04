0381ddbad9ba3cd6cc356bedde8238b2d41c109a
ButlerFuqua <butlerfuqua@gmail.com>
2022-02-04 11:56:05 -1000

    Remove js files

--- src/persistance/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.siteDb = void 0;
-const SiteDb_1 = require("./SiteDb");
-exports.siteDb = new SiteDb_1.SiteDb();
